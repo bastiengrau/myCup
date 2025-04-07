318c7ac3fc6c44f9a7454deb5c71b887559efe50
Bastien <bastien.grau@gmail.com>
2025-04-07 16:54:13 +0200

    init add team vue

---
+++ vue/AddTeam.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Ajout Equipe</title>
+</head>
+<body>
+<div class="card" >
+    <div class="card-header" style="background-color: dimgrey; color: white">
+        Ajouter une équipe
+    </div>
+    <div class="card-body">
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" id="club" placeholder="nom du club">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="teamName" placeholder="nom de l'équipe">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
+</body>
+</html>
+
+
+<!--<html>
+<body>
+<div class="container">
+    <form>
+        <div class="row">
+            <div class="col">
+                <input class="form-control" type="text" id="club" placeholder="club">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <input class="form-control" type="text" id="team" placeholder="équipe">
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>-->
